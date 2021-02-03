d02185340fcbfde2ea0d31ce1dae4e54f09d6623
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2021-02-03 10:53:41 +0100

    Impl Map based Services, closes issue No.2

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,9 +1,5 @@
 package guru.springframework.sfgpetclinic.services;
-
 import guru.springframework.sfgpetclinic.model.Owner;
-
-
-
 public interface OwnerService extends CrudService<Owner,Long>{
     Owner findByLastName(String lastName);
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgpetclinic.services;
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
-import java.util.Set;
 public interface PetService extends CrudService<Pet,Long>{
-
 
 }
